c666389531df5f7d2b7c2330f5686ea99cf4e93a
Jamal Tarafdar <jamal.tarafdar@gmail.com>
2020-02-08 21:27:23 +0000

    use react + typescript

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,23 +1,26 @@
 import path from "path";
 import webpack from "webpack";
 import HTMLWebpackPlugin from "html-webpack-plugin";
-
+import ForkTsCheckerWebpackPlugin from "fork-ts-checker-webpack-plugin";
 
 const config: webpack.Configuration = {
+    context: __dirname,
     mode: 'production',
-    entry: './src/index.js',
+    entry: './src/index.tsx',
     output: {
         path: path.resolve(__dirname, 'dist'),  // need to define absolute path
         filename: 'app.bundle.js'
     },
+    resolve:{
+        extensions: ['.ts', '.tsx', '.js']
+    },
     module:{
         rules:[
             {
-                test: /\.js$/,
-                loader: 'babel-loader',
-                exclude: /node_modules/,
+                test: /\.tsx?$/,
+                loader: 'ts-loader',
                 options:{
-                    presets:['@babel/preset-env', '@babel/preset-react']
+                    transpileOnly: true
                 }
             }
         ]
@@ -25,7 +28,8 @@ const config: webpack.Configuration = {
     plugins:[
         new HTMLWebpackPlugin({
             template: './src/index.html'
-        })
+        }),
+        new ForkTsCheckerWebpackPlugin(),
     ]
 
 }
