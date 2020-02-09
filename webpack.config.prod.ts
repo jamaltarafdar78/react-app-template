38a246acb0c1eca01b099efd0a8987225124ea2d
Jamal Tarafdar <jamal.tarafdar@gmail.com>
2020-02-09 13:01:34 +0000

    externalise react libs to use CDN in production bundles

--- webpack.config.prod.ts
+++ webpack.config.prod.ts
@@ -1,12 +1,19 @@
 import merge from "webpack-merge";
 import baseConfig from "./webpack.config.base";
 import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
+import {Configuration} from "webpack";
 
-export default merge(baseConfig, {
+const prodOverrides: Partial<Configuration> = {
     mode: 'production',
     plugins: [
         new BundleAnalyzerPlugin({
             analyzerMode: 'static'
         })
-    ]
-})
+    ],
+    externals: {
+        react: 'React',
+        'react-dom': 'ReactDOM' 
+    }
+}
+
+export default merge(baseConfig,prodOverrides)
