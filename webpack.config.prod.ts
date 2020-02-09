9d0cbcc9af21dca7b0eb27695b6dbbe49b79283d
Jamal Tarafdar <jamal.tarafdar@gmail.com>
2020-02-09 12:46:38 +0000

    add webpackBundleAnalyzer to production webpack

--- webpack.config.prod.ts
+++ webpack.config.prod.ts
@@ -1,6 +1,12 @@
 import merge from "webpack-merge";
 import baseConfig from "./webpack.config.base";
+import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
 
 export default merge(baseConfig, {
     mode: 'production',
+    plugins: [
+        new BundleAnalyzerPlugin({
+            analyzerMode: 'static'
+        })
+    ]
 })
