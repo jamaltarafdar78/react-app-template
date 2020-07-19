1e0cd5c62dc768e8d9ce08cf822eb497ee543a92
Jamal Tarafdar <jamal.tarafdar@gmail.com>
2020-07-19 19:53:38 +0100

    testing add locally running app on Ipad and Iphone devices (webkit only)

--- xbrowser/webkit.js
+++ xbrowser/webkit.js
@@ -1,19 +1,29 @@
 const playwright = require('playwright');
 
-(async () => {
-    for (const browserType of ['chromium', 'firefox', 'webkit']) {
-        const browser = await playwright[browserType].launch();
+(async devicesToTest => {
+    for (const device of devicesToTest) {
+        for (const browserType of ['webkit']) {
+            const browser = await playwright[browserType].launch();
 
-        const context = await browser.newContext();
+            const context = await browser.newContext({
+                ...device,
+            });
 
-        const page = await context.newPage();
-        await page.goto('http://localhost:8080/');
+            const page = await context.newPage();
+            await page.goto('http://localhost:8080/');
 
-        await page.screenshot({
-            path: `./xbrowser/screenshots/${browserType}.png`,
-            fullPage: true,
-        });
+            await page.screenshot({
+                path: `./xbrowser/screenshots/${browserType}-${device.id}.png`,
+                fullPage: true,
+            });
 
-        await browser.close();
+            await browser.close();
+        }
     }
-})();
+})([
+    {
+        ...playwright.devices['iPad (gen 7) landscape'],
+        id: 'iPad_landscape_gen7',
+    },
+    { ...playwright.devices['iPhone X'], id: 'iPhoneX' },
+]);
